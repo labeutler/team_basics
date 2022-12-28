4ff5f38a9d609424758d65fe61427a50e3e73826
Laura Beutler <labeutler@gmail.com>
2022-12-28 12:51:45 -0500

    create employee.js info

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+//list of what employees are needed
 const Employee = require('./lib/employee');
+//team info 
 const engineer = require('./lib/engineer');
 const intern = require('./lib/intern');
 const manager = require('./lib/manager');
 
-const employee = 
+const employee = cd
